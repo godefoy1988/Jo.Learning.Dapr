8dea46f619fc790c6f0830179bee7b63f3577c21
Jose Luis <godefoy1988@gmail.com>
2021-03-05 11:34:42 -0500

    initial commit...

---
+++ src/Jo.Learning.Dapr.App1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jo.Learning.Dapr.App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
